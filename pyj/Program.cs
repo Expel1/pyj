3ab51586ea7735bfd3fabea7fd8655920cb59e50
Expel1 <78724771+Expel1@users.noreply.github.com>
2022-01-10 21:08:19 +0100

    Add project files.

---
+++ pyj/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace pyj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Penis\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
